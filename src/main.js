f262695bda403d924a65a38595a32e0bb9675246
vickytlwg <1282356934@qq.com>
2020-02-17 17:29:46 +0800

    Signed-off-by: Vicky <1251658027@qq.com>

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router.js'
+import './plugins/element.js'
 
 Vue.config.productionTip = false
 
