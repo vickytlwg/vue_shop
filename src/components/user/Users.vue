5688ef8fd312af3d04e16f71a84ccc4b352dc1ea
vickytlwg <1282356934@qq.com>
2020-02-25 07:56:07 +0800

    用户管理

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -10,12 +10,13 @@
         <el-card class="box-card">
             <el-row :gutter="20">
                 <el-col :span="8">
-                    <el-input placeholder="请输入内容" class="input-with-select">
-                        <el-button slot="append" icon="el-icon-search"></el-button>
+                    <el-input placeholder="请输入内容" v-model="queryInfo.query" class="input-with-select" clearable
+                              @clear="getUserList">
+                        <el-button slot="append" icon="el-icon-search" @click="getUserList"></el-button>
                     </el-input>
                 </el-col>
                 <el-col :span="4">
-                    <el-button type="primary">添加用户</el-button>
+                    <el-button type="primary" @click="addDialogVisible=true">添加用户</el-button>
                 </el-col>
             </el-row>
             <el-table border stripe
@@ -47,14 +48,14 @@
                         label="状态">
                     <template slot-scope="scope">
                         <el-switch
-                                v-model="scope.row.mg_state">
+                                v-model="scope.row.mg_state" @change="userStateChange(scope.row)">
                         </el-switch>
                     </template>
                 </el-table-column>
                 <el-table-column width="180"
                                  label="操作">
                     <template slot-scope="scope">
-                        <el-button type="primary" icon="el-icon-edit" size="mini" circle></el-button>
+                        <el-button type="primary" icon="el-icon-edit" size="mini" @click="showEditDialog()" circle></el-button>
                         <el-button type="danger" icon="el-icon-delete" size="mini" circle></el-button>
                         <el-tooltip class="item" effect="dark" content="分配角色" placement="top-end" :enterable="false">
                             <el-button type="warning" icon="el-icon-setting" size="mini" circle></el-button>
@@ -72,6 +73,29 @@
                     :total="total">
             </el-pagination>
         </el-card>
+        <el-dialog
+                title="添加用户"
+                :visible.sync="addDialogVisible"
+                width="30%" @close="addDialogClosed">
+            <el-form :model="addForm" :rules="addFormRules" ref="addFormRef" label-width="70px">
+                <el-form-item label="用户名" prop="username">
+                    <el-input v-model="addForm.username"></el-input>
+                </el-form-item>
+                <el-form-item label="密码" prop="password">
+                    <el-input v-model="addForm.password"></el-input>
+                </el-form-item>
+                <el-form-item label="邮箱" prop="email">
+                    <el-input v-model="addForm.email"></el-input>
+                </el-form-item>
+                <el-form-item label="手机" prop="mobile">
+                    <el-input v-model="addForm.mobile"></el-input>
+                </el-form-item>
+            </el-form>
+            <span slot="footer" class="dialog-footer">
+    <el-button @click="addDialogVisible = false">取 消</el-button>
+    <el-button type="primary" @click="addUser">确 定</el-button>
+  </span>
+        </el-dialog>
     </div>
 </template>
 
@@ -79,15 +103,56 @@
     export default {
         name: "Users",
         data() {
+            var checkEmail = (rule, value, callback) => {
+                const regEmail=/^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(\.[a-zA-Z0-9_-])+/
+                if (regEmail.test(value)){
+                    return callback()
+                }
+                callback(new Error('请输入合法的邮箱'))
+            }
+            var checkMobile = (rule, value, callback) => {
+                const regMobile=/^(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$/
+                if (regMobile.test(value)){
+                    return callback()
+                }
+                callback(new Error('请输入合法的号码'))
+            }
             return {
                 //获取用户列表的参数
                 queryInfo: {
                     query: '',
                     pagenum: 1,
-                    pagesize: 10,
+                    pagesize: 2,
                 },
                 userlist: [],
-                total: 0
+                total: 0,
+                addDialogVisible: false,//控制对话框可见
+                //添加用户的表单数据
+                addForm: {
+                    username: '',
+                    password: '',
+                    email: '',
+                    mobile: ''
+                },
+                //添加表单的验证规则
+                addFormRules: {
+                    username: [
+                        {required: true, message: '请输入用户名', trigger: 'blur'},
+                        {min: 3, max: 10, message: '用户名的长度在3-10个字符内'}
+                    ],
+                    password: [
+                        {required: true, message: '请输入密码', trigger: 'blur'},
+                        {min: 6, max: 15, message: '用户名的长度在6-15个字符内'}
+                    ],
+                    email: [
+                        {required: true, message: '请输入邮箱',trigger: 'blur' },
+                        {validator: checkEmail,trigger: 'blur' }
+                    ],
+                    mobile: [
+                        {required: true, message: '请输入手机', trigger: 'blur'},
+                        {validator: checkMobile, trigger: 'blur'}
+                    ],
+                }
             }
         },
         created() {
@@ -106,14 +171,44 @@
             //监听pagesize变化的事件
             handleSizeChange(newSize) {
                 console.log(newSize)
-                this.queryInfo.pagesize=newSize
+                this.queryInfo.pagesize = newSize
                 this.getUserList()
             },
             //监听页码值改变的事件
             handleCurrentChange(newPage) {
                 console.log(newPage)
-                this.queryInfo.pagenum=newPage
+                this.queryInfo.pagenum = newPage
                 this.getUserList()
+            },
+            //监听switch用户状态
+            async userStateChange(userinfo) {
+                console.log(userinfo)
+                const {data: res} = await this.$http.put(`users/${userinfo.id}/state/${userinfo.mg_state}`)
+                if (res.meta.status !== 200) {
+                    userinfo.mg_state = !userinfo.mg_state
+                    return this.$message.error('更新用户状态失败!')
+                }
+                this.$message.success('更新用户成功!')
+            },
+            //close对话框之后清空事件
+            addDialogClosed(){
+                this.$refs.addFormRef.resetFields()
+            },
+            //添加用户
+            addUser(){
+                this.$refs.addFormRef.validate(async valid=>{
+                    if(!valid) return
+                    const { data:res}=await this.$http.post('users',this.addForm)
+                    if(res.meta.status!==201){
+                        this.$message.error("添加用户失败!")
+                    }
+                    this.$message.success('添加用户成功!')
+                    this.addDialogVisible=false
+                    this.getUserList()
+                })
+            },
+            //修改
+            showEditDialog(){
 
             }
         }
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -22,6 +22,7 @@ import {
     Switch,
     Tooltip,
     Pagination,
+    Dialog,
 } from "element-ui"
 
 Vue.use(Button)
@@ -46,3 +47,4 @@ Vue.use(TableColumn)
 Vue.use(Switch)
 Vue.use(Tooltip)
 Vue.use(Pagination)
+Vue.use(Dialog)
