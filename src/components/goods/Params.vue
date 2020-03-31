f07daa51b76a25c9c565e175132b008ace171d4a
vickytlwg <1282356934@qq.com>
2020-03-31 17:14:04 +0800

    half

---
+++ src/components/goods/Params.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+    <!--面包屑导航区-->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+        <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+        <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+        <el-breadcrumb-item>分类参数</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card class="box-card">
+        <div slot="header" class="clearfix">
+            <span>卡片名称</span>
+            <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+        </div>
+        <div v-for="o in 4" :key="o" class="text item">
+            {{"列表内容 " + o }}
+        </div>
+    </el-card>
+</div>
+</template>
+
+<script>
+  export default {
+    name: "Params"
+  }
+</script>
+
+<style scoped>
+
+</style>
